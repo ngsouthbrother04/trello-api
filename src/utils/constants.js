2a8465e772fac68c00fc531a6fbe0ba1720d6f32
Nguyen Anh <nguyennam4c7@gmail.com>
2025-12-12 11:14:31 +0700

    /update: Update cors config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
 // Những domain được phép truy cập tài nguyên từ server
-export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
-  'https://trello-api-services.onrender.com'
-]
+export const WHITELIST_DOMAINS = []
 
 export const BOARD_TYPE = {
   PUBLIC: 'public',
