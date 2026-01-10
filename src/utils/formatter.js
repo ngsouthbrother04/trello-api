bbc94e579a93c308e51a9c706797d65c8ec8f291
Nguyen Anh <nguyennam4c7@gmail.com>
2026-01-10 15:17:39 +0700

    Fix user styling and formatter

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,4 +1,10 @@
+import { pick } from 'lodash'
+
 //Convert a string to a URL-friendly slug
+/**
+ * Original String Test: 'TrungQuanDev Một Lập Trình Viên'
+ * Slug Result: trungquandev-mot-lap-trinh-vien
+ */
 export const slugify = (val) => {
   if (!val) return ''
   return String(val)
@@ -11,7 +17,7 @@ export const slugify = (val) => {
     .replace(/-+/g, '-') // remove consecutive hyphens
 }
 
-/**
- * Original String Test: 'TrungQuanDev Một Lập Trình Viên'
- * Slug Result: trungquandev-mot-lap-trinh-vien
- */
+export const pickUserData = (user) => {
+  if (!user) return {}
+  return pick(user, ['_id', 'email', 'username', 'displayName', 'avatar', 'role', 'isActive', 'createdAt', 'updatedAt'])
+}
